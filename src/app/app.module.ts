1b51bcf7ec5860433e1db0cae5112d515d121b63
Boamorte8 <salazar.z.esteban@gmail.com>
2018-10-28 11:32:12 -0500

    Se agrega protección de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -22,6 +22,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,13 @@ import { AuthService } from './auth/auth.service';
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,18 +4,21 @@ import { throwError } from 'rxjs';
 import { Http, Response } from '@angular/http';
 import { map, catchError } from 'rxjs/operators';
 
+import { AuthService } from './../auth/auth.service';
 import { RecipeService } from './../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  private DB_RECIPES_URL = 'https://ng-recipe-book-2914a.firebaseio.com/Recipes.json';
+  private DB_RECIPES_URL = 'https://ng-recipe-book-2914a.firebaseio.com/Recipes.json?auth=';
 
-  constructor(private http: Http, private recipeService: RecipeService) { }
+  constructor(private http: Http, private recipeService: RecipeService,
+              private authService: AuthService) { }
 
   storeRecipes() {
-    return this.http.put(this.DB_RECIPES_URL, this.recipeService.getRecipes())
+    const token = this.authService.getToken();
+    return this.http.put(this.DB_RECIPES_URL + token, this.recipeService.getRecipes())
       .pipe(map(
         (response: Response) => {
           return response;
@@ -26,7 +29,8 @@ export class DataStorageService {
   }
 
   getRecipes() {
-    this.http.get(this.DB_RECIPES_URL).pipe(
+    const token = this.authService.getToken();
+    this.http.get(this.DB_RECIPES_URL + token).pipe(
       map((response: Response) => {
         const recipes: Recipe[] = response.json();
         for (const recipe of recipes) {
