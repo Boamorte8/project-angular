e0e12ecb27def5aea86660dc36f9d37ab53558fc
Boamorte8 <salazar.z.esteban@gmail.com>
2018-10-31 21:31:49 -0500

    Se comienza el módulo de redux

---
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,24 @@
+import * as ShoppingListActions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Potatoes', 15),
+    new Ingredient('Bread', 1),
+    new Ingredient('Pasta', 150),
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
+  }
+}
